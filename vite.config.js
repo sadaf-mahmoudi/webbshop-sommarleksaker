8d78df21373b015f7b4a64f0f621d8ac830ae563
sadaf-mahmoudi <sadafm2003@gmail.com>
2024-12-02 14:17:03 +0100

    updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/webbshop-sommarleksaker/',
+  build: {
+    outDir: 'dist',
+    sourcemap: true
+  }
 })
